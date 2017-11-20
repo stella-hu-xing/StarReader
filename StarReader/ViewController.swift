8ca0b8c8ef3a1aa55e1dfb7a86cc556500021216
Xing Hu <xinghu0164@gmail.com>
2017-11-20 17:24:32 +1100

    change "document based app" project to "single view" project

---
+++ StarReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarReader
+//
+//  Created by Xing Hu on 20/11/17.
+//  Copyright © 2017 Xing Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
